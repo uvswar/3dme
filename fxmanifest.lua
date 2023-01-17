afed606503282c2137f0cc0210fd738f775ad61d
Don <36248274+Donthelosr@users.noreply.github.com>
2023-01-17 18:47:37 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Donthelosr'
+description '/me command but it\'s 3D printed'
+version '3.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
